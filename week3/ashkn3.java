806f492796e031db9abac07363b3994664cb9873
Ayesha <ashkn55@gmail.com>
2026-09-04 20:59:33 +0530

    Update ashkn3.java

--- week3/ashkn3.java
+++ week3/ashkn3.java
@@ -5,6 +5,6 @@ public class ashkn3 {
     public static void main(String args[]) {
         int i = 2;
         int cube = i*i*i;
-        System.out.println("The cube of " + "is " + cube);
+        System.out.println("The cube of " + i + " is " + cube);
     }
 }
